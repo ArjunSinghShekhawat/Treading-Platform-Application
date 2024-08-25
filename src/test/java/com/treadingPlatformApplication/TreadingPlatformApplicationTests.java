d79f0ea54a2de2e5654a4a17f74d2503cc438bcb
Arjun Singh Shekhawat <shekhawatsingharjun12345@gmail.com>
2024-08-25 20:18:42 +0530

    first commit

---
+++ src/test/java/com/treadingPlatformApplication/TreadingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treadingPlatformApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreadingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
